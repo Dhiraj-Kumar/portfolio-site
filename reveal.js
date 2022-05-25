6b888893d0f6308551e26e978725f087ffa51796
Dhiraj Kumar <dhiraj.kumar@stackroute.in>
2022-05-25 23:42:26 +0530

    updated

---
+++ reveal.js
@@ -0,0 +1,66 @@
+ScrollReveal().reveal('#me', {
+    distance: '15%',
+    origin: 'right',
+    opacity: 0,
+    duration: 1500
+});
+ScrollReveal().reveal('#intro', {
+    distance: '15%',
+    origin: 'left',
+    opacity: 0,
+    duration: 1500
+});
+ScrollReveal().reveal('.mynavbar', {
+    distance: '15%',
+    origin: 'top',
+    opacity: 0,
+    duration: 1500
+});
+ScrollReveal().reveal('#me2', {
+    distance: '15%',
+    origin: 'left',
+    opacity: 0,
+    duration: 1500
+});
+ScrollReveal().reveal('#profile', {
+    distance: '15%',
+    origin: 'right',
+    opacity: 0,
+    duration: 1500
+});
+ScrollReveal().reveal('.card', {
+    distance: '15%',
+    origin: 'bottom',
+    opacity: 0,
+    duration: 2000
+});
+ScrollReveal().reveal('#iamhere', {
+    distance: '15%',
+    origin: 'left',
+    opacity: 0,
+    duration: 1500
+});
+ScrollReveal().reveal('#challenge', {
+    distance: '15%',
+    origin: 'right',
+    opacity: 0,
+    duration: 1500
+});
+ScrollReveal().reveal('#skillrow1', {
+    distance: '15%',
+    origin: 'right',
+    opacity: 0,
+    duration: 1500
+});
+ScrollReveal().reveal('#skillrow2', {
+    distance: '15%',
+    origin: 'left',
+    opacity: 0,
+    duration: 1500
+});
+ScrollReveal().reveal('#skillrow3', {
+    distance: '15%',
+    origin: 'right',
+    opacity: 0,
+    duration: 1500
+});
